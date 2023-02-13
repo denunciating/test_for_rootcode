ac86f5935748d82379b610f08883c9bbdc1dafdf
denunciating <125296582+denunciating@users.noreply.github.com>
2023-02-13 19:57:13 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,48 @@
+import psycopg2
+import db_info
+import numpy as np
+from matplotlib import pyplot as plt
+from matplotlib.animation import FuncAnimation
+
+table = psycopg2.connect(
+    host=db_info.host,
+    user=db_info.user,
+    password=db_info.password,
+    database=db_info.database
+)
+select = f'SELECT * FROM foucault'
+with table.cursor() as cursor:
+    cursor.execute(select)
+    result = cursor.fetchall()
+
+g = float(result[0][0])
+L = float(result[0][1])
+init_x = float(result[0][2])
+init_y = float(result[0][3])
+init_xdot = float(result[0][4])
+init_ydot = float(result[0][5])
+omega = float(result[0][6])
+lamb = float(result[0][7])
+
+fig = plt.figure()
+lines = plt.plot([], '.', lw=0.1)
+line = lines[0]
+plt.axis('scaled')
+plt.xlim(-0.7, 0.7)
+plt.ylim(-0.7, 0.7)
+
+x = list()
+y = list()
+
+
+def animate(frame):
+    t = frame*10
+    x0 = init_x * np.cos(g/L * t) * np.cos(omega * np.sin(lamb) * t)
+    x.append(x0)
+    y0 = -init_x * np.cos(g/L * t) * np.sin(omega * np.sin(lamb) * t)
+    y.append(y0)
+    line.set_data((x, y))
+
+
+anim = FuncAnimation(fig, animate, frames=8640, interval=1)
+plt.show()
---
+++ xls_to_sql.py
@@ -0,0 +1,52 @@
+import psycopg2
+import db_info
+import pandas as pd
+
+
+# try:
+#     with psycopg2.connect(
+#         host=db_info.host,
+#         user=db_info.user,
+#         password=db_info.password,
+#         database=db_info.database
+#     ) as connection:
+#         connection.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
+#         create_db_query = 'CREATE DATABASE IF NOT EXISTS foucault'
+#         with connection.cursor() as cursor:
+#             cursor.execute(create_db_query)
+# except Exception:
+#     print('Creation is failed. ')
+
+
+table = psycopg2.connect(
+    host=db_info.host,
+    user=db_info.user,
+    password=db_info.password,
+    database=db_info.database
+)
+drop_table_query = 'DROP TABLE IF EXISTS foucault'
+create_table_query = 'CREATE TABLE IF NOT EXISTS foucault(' \
+                     'g NUMERIC(7,3) NOT NULL,' \
+                     'L NUMERIC(7,3) NOT NULL,' \
+                     'init_x NUMERIC(5,3) NOT NULL,' \
+                     'init_y NUMERIC(5,3),' \
+                     'init_xdot NUMERIC(5,3),' \
+                     'init_ydot NUMERIC(5,3),' \
+                     'omega NUMERIC(17,12) NOT NULL,' \
+                     'lambda NUMERIC(17,12) NOT NULL);'
+
+insert_query = 'INSERT INTO foucault(g, L, init_x, init_y, init_xdot, ' \
+               'init_ydot, omega, lambda) VALUES (%s,%s,%s,%s,%s,%s,%s,%s)'
+
+file = 'data.xlsx'
+xl = pd.read_excel(file)
+args = (xl['g'][0], xl['L'][0], xl['init_x'][0], xl['init_y'][0],
+             xl['init_xdot'][0], xl['init_ydot'][0],
+             xl['omega'][0], xl['lambda'][0])
+
+with table.cursor() as cursor:
+    cursor.execute(drop_table_query)
+    cursor.execute(create_table_query)
+    cursor.execute(insert_query, args)
+    table.commit()
+
