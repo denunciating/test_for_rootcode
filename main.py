08b0ee598a9107371ace5832696e255596767e7c
denunciating <125296582+denunciating@users.noreply.github.com>
2023-02-15 20:34:06 +0300

    Update main.py

--- main.py
+++ main.py
@@ -15,14 +15,7 @@ with table.cursor() as cursor:
     cursor.execute(select)
     result = cursor.fetchall()
 
-g = float(result[0][0])
-L = float(result[0][1])
-init_x = float(result[0][2])
-init_y = float(result[0][3])
-init_xdot = float(result[0][4])
-init_ydot = float(result[0][5])
-omega = float(result[0][6])
-lamb = float(result[0][7])
+g, L, init_x, init_y, init_xdot, init_ydot, omega, lamb = [float(result[0][i]) for i in range(8)]
 
 fig = plt.figure()
 lines = plt.plot([], '.', lw=0.1)
@@ -41,8 +34,8 @@ y = list()
 def animate(frame):
     t = frame*10
     x0 = init_x * np.cos(g/L * t) * np.cos(omega * np.sin(lamb) * t)
-    x.append(x0)
     y0 = -init_x * np.cos(g/L * t) * np.sin(omega * np.sin(lamb) * t)
+    x.append(x0)
     y.append(y0)
     line.set_data((x, y))
 
